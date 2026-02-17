fa0f172b3907847a67ae9fc47db16d96f5d2fba7
admwhscpi-blip <admwhs.cpi@gmail.com>
2026-02-17 21:38:43 +0700

    9

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'whsmart-cache-v18.34';
+const CACHE_NAME = 'whsmart-cache-v18.35';
 const urlsToCache = [
     './entry-downtime.html',
     'https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css',
